173a6f67b8c86dd98792e86f2b8b33b0f8b893f3
Austin Brown <atb.austin@gmail.com>
2023-04-19 16:29:28 -0500

    HiddenStats Component (#31)

---
+++ src/component/widget/statistic/HiddenStats.tsx
@@ -0,0 +1,34 @@
+import "./Stats.css";
+import React from "react";
+
+/**
+ * A component that is the "hidden" banner, which is basically a button that
+ * would, in context, expand back to the full stats ribbon.
+ *
+ * @return {ReactElement} React Component
+ */
+export default function Stats({ onClick }: HiddenStatsProps) {
+  const leftArrow = "◀";
+  return (
+    <div className="Stats Stats-Container">
+      <div
+        className="Stats Stats-Visual"
+        data-testid="stats-ribbon-hidden"
+        onClick={onClick}
+        style={{
+          aspectRatio: 1,
+          justifyContent: "center",
+        }}
+      >
+        {leftArrow}
+      </div>
+    </div>
+  );
+}
+
+/**
+ * Properties for the HiddenStats component.
+ */
+type HiddenStatsProps = {
+  onClick: () => void;
+};
